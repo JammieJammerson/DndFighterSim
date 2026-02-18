3398a1a9c5f00d927096392ed3ffef1983080796
Hammonds <Hammonds@DESKTOP-MIFLJ5F>
2026-02-18 14:45:52 -0500

    Add project files.

---
+++ DndFighterSim/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DndFighterSim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Full Name and the Campaign:");
+            Console.WriteLine("Full Name:");
+            Console.ReadLine();
+            Console.WriteLine("Press any Key to Continue:");
+            Console.ReadKey();
+            Console.WriteLine("Campaign:");
+            Console.ReadLine();
+            Console.ReadKey();
+            Console.WriteLine("Give Me The Characters and Inititives:");
+            Console.ReadLine();
+
+        }
+    }
+}
