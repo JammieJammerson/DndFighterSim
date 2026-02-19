f5a35a2e3838f930941fc5d73475be6a2e6cc79b
Hammonds <Hammonds@DESKTOP-MIFLJ5F>
2026-02-18 21:17:27 -0500

    Added Character placeholder classes

---
+++ DndFighterSim/CharacterStats.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class DungeonsandDragons
+{
+    public Artificer()
+    {
+    }
+    public Barbarian()
+	{
+	}
+    public Bard()
+    {
+    }
+    public Barbarian()
+    {
+    }
+    public Cleric()
+    {
+    }
+    public Druid()
+    {
+    }
+    public Fighter()
+    {
+    }
+    public Monk()
+    {
+    }
+    public Paladin()
+    {
+    }
+    public Ranger()
+    {
+    }
+    public Rogue()
+    {
+    }
+    public Sorcerer()
+    {
+    }
+    public Warlock()
+    {
+    }
+    public Wizard()
+    {
+    }
+}
--- DndFighterSim/Program.cs
+++ DndFighterSim/Program.cs
@@ -8,6 +8,10 @@ namespace DndFighterSim
 {
     internal class Program
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Enter your Name and the Campaign:");
