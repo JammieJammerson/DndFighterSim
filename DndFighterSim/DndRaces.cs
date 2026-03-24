8d8444d89c0b199d0fc8982fdea728991561b97a
Jacob Hammonds <jimmydhambone@gmail.com>
2026-03-24 14:04:30 -0400

    Add abstract properties for the rest of the DnD races

--- DndFighterSim/DndRaces.cs
+++ DndFighterSim/DndRaces.cs
@@ -9,26 +9,42 @@ public abstract class DndRaces
     }
     public abstract Dwarf()
     {
+		get { return Racename; }
+		set { Racename = value;}
     }
     public abstract Elf()
     {
+		get { return Racename; }
+		set { Racename = value;}
     }
     public abstract Gnome()
     {
+		get { return Racename; }
+		set { Racename = value;}
     }
     public abstract HalfElf()
     {
+		get { return Racename; }
+		set { Racename = value;}
     }
     public abstract HalfOrc()
     {
+		get { return Racename; }
+		set { Racename = value;}
     }
     public abstract Halfling()
     {
+		get { return Racename; }
+		set { Racename = value;}
     }
     public abstract Human()
     {
+		get { return Racename; }
+		set { Racename = value;}
     }
     public abstract Tiefling()
 	{
+		get { return Racename; }
+		set { Racename = value;}
 	}
 }
